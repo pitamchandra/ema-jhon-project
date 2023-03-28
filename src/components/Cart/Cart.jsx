1dcdb62fe53d8192ecb0902764bcccbe78575240
pitamchandra <pitamchandra402@gmail.com>
2023-03-28 20:40:43 +0600

    show date in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,17 +5,22 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        totalPrice += product.price;
-        totalShipping += product.shipping;
-        console.log(product);
+        // product.quantity = product.quantity || 1;
+        if(product.quantity === 0){
+            product.quantity = 1;
+        }
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice * 7 /100; 
     const grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='cart'>
             <h2>Order summary</h2>
-            <p>selected item : {cart.length}</p>
+            <p>selected item : {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${tax}</p>
